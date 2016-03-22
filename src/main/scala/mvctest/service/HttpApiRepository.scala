d8dc0bc3e1c492daf14e18811abccd0a799c2a4a
一剑 <yijian@wacai.com>
2016-03-22 15:56:54 +0800

    @Query 返回List 需要写成 java.util.List[String] 不是直接用的List（默认是scala.collections.immutable.List）

--- src/main/scala/mvctest/service/HttpApiRepository.scala
+++ src/main/scala/mvctest/service/HttpApiRepository.scala
@@ -9,6 +9,6 @@ import javax.persistence.Persistence
 import mvctest.domain.HttpApi
 
 trait HttpApiRepository extends PagingAndSortingRepository[HttpApi, Long] {
-  @Query(value = "SELECT distinct product FROM hotel.http_api limit 1", nativeQuery = true)
-  def findProducts(): String
+  @Query(value = "SELECT distinct product FROM hotel.http_api", nativeQuery = true)
+  def findProducts(): java.util.List[String]
 }
