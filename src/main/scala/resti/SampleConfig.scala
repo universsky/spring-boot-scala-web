401fe0e181fbab267174b986a1a72caf2f975fef
一剑 <yijian@wacai.com>
2016-03-25 00:40:40 +0800

    prePostAthentication do not affects

--- src/main/scala/resti/SampleConfig.scala
+++ src/main/scala/resti/SampleConfig.scala
@@ -3,8 +3,10 @@ package resti
 import org.springframework.context.annotation.Configuration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 
 @Configuration
 @EnableAutoConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan
 class SampleConfig
